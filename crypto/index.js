aa6a27e37142df3e3a365b99144e300ccdfa989c
Aleksander Hjortkær Sand Evensen (Elev) <aleksandhe@viken.no>
2020-12-05 00:28:44 +0100

    Added some more testing files to the Crypto website

--- crypto/index.js
+++
@@ -1,8 +0,0 @@
-const fetchCrypto = async (symbol, apiKey, currency = 'USD') => {
-    return await fetch(`https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=${symbol}&market=${currency}&apikey=${apiKey}`).then(res => res.json());
-}
-
-
-(async ()=> {
-    console.log(await fetchCrypto('BTC', 'TUTBF3A8SG23ZWCJ', 'NOK'))
-})()
